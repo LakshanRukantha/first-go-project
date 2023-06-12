1184083db0a0e38ea8e8a4c5dcd7cf21e0b2545d
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-06-12 22:39:44 +0530

    Learn to use external packages

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import "fmt"
+import "rsc.io/quote"
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
 	// var name string = "Lakshan Rukantha"
 	// var age int = 22
 
-	name := "Lakshan Rukantha"
-	age := 22
-	weight := 55.25
+	// name := "Lakshan Rukantha"
+	// age := 22
+	// weight := 55.25
 
-	fmt.Printf("I am %v and I am %v years old.\n", name, age)
-	fmt.Printf("Weight: %g Kg", weight)
+	// fmt.Printf("I am %v and I am %v years old.\n", name, age)
+	// fmt.Printf("Weight: %g Kg", weight)
 
+	fmt.Println(quote.Go())
 
 }
