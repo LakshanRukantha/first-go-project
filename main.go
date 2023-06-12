89bc041a3da90b554e90c37cfaff26350890e5fc
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-06-12 22:27:11 +0530

    Variables and format specifiers

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	// var name string = "Lakshan Rukantha"
+	// var age int = 22
+
+	name := "Lakshan Rukantha"
+	age := 22
+	weight := 55.25
+
+	fmt.Printf("I am %v and I am %v years old.\n", name, age)
+	fmt.Printf("Weight: %g Kg", weight)
+
+
 }
